09652aa4c5fb4f5bcd5835f5f9d2df8a2132b541
Stanislav Dzisiak <stanislav.dzisiak@outlook.com>
2019-06-11 00:22:34 +0300

    initial commit

---
+++ Makefile
@@ -0,0 +1,30 @@
+install:
+	npm install
+
+publish:
+	npm login
+	npm publish --dry-run
+
+build:
+	npm run build
+
+build-dev:
+	npm run build-dev
+
+develop:
+	npm run develop
+
+lint:
+	npx eslint .
+
+test:
+	npm test
+
+test-coverage:
+	npm test -- --coverage
+
+test-watch:
+	npm test -- --watch
+
+make deploy:
+	npm run deploy
---
+++ webpack.config.babel.js
@@ -0,0 +1,36 @@
+import HtmlWebpackPlugin from 'html-webpack-plugin';
+import { CleanWebpackPlugin } from 'clean-webpack-plugin';
+
+module.exports = {
+  mode: process.env.NODE_ENV || 'development',
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: '/node_modules/',
+        loader: 'babel-loader',
+      },
+      {
+        test: /\.css$/,
+        use: ['style-loader/url', 'file-loader'],
+      },
+    ],
+  },
+  plugins: [
+    new CleanWebpackPlugin(),
+    new HtmlWebpackPlugin({
+      template: 'src/template.html',
+      minify: {
+        collapseWhitespace: true,
+        removeScriptTypeAttributes: true,
+      },
+    }),
+  ],
+  devtool: process.env.NODE_ENV ? '' : 'source-map',
+  devServer: {
+    overlay: {
+      warnings: true,
+      errors: true,
+    },
+  },
+};
